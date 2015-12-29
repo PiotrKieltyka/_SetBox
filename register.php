426a494aa1e9a2028eea8bfb06526d6cdeecb52d
Piotrek <piotr.kieltyka@pjwstk.edu.pl>
2015-12-29 17:30:54 +0100

    ver 0.92

--- register.php
+++ register.php
@@ -2,7 +2,7 @@
     session_start();
 
     if(isset($_POST['email'])) {
-        // Udana walidacja
+
         $wszystko_OK = true;
 
         $login = $_POST['login'];
@@ -22,9 +22,38 @@
 
         if(!isset($_POST['regulamin'])) { $wszystko_OK = false; $_SESSION['e_regulamin'] = "Potwierdź akceptację regulaminu"; }
 
-        if($wszystko_OK == true) {
-            exit();
-        }
+        require_once "connect.php";
+        mysqli_report(MYSQLI_REPORT_STRICT);
+        try { $polaczenie = new mysqli($host, $db_user, $db_password, $db_name);
+            if($polaczenie->connect_errno!=0) { throw new Exception(mysqli_connect_errno()); }
+            else {
+                $rezultat = $polaczenie->query("SELECT id FROM users WHERE email='$email'");
+                if(!$rezultat) throw new Exception($polaczenie->error);
+                $ile_takich_maili = $rezultat->num_rows;
+                if($ile_takich_maili>0) { $wszystko_OK = false; $_SESSION['e_email'] = "Istnieje już konto przypisane do tego adresu e-mail"; }
+
+                $rezultat = $polaczenie->query("SELECT id FROM users WHERE user='$login'");
+                if(!$rezultat) throw new Exception($polaczenie->error);
+                $ile_takich_loginow = $rezultat->num_rows;
+                if($ile_takich_loginow>0) { $wszystko_OK = false; $_SESSION['e_login'] = "Istnieje już konto przypisane do tego loginu"; }
+
+                if($wszystko_OK == true) {
+                    if($polaczenie->query("INSERT INTO users VALUES(NULL, '$login', '$haslo_hash', '$email', 14)")) {
+                        $_SESSION['udana_rejestracja'] = true;
+                        header('Location:index.php');
+                    }
+                    else {
+                        throw new Exception($polaczenie->error);
+                    }
+                }
+
+                $polaczenie->close();
+            }
+
+            }
+        catch (Exception $e) {
+            echo '<span style="color: red;">Blad serwera';
+            }
     }
 ?>
 
